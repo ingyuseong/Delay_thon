79351e9ab0c7a8fbbf2e2af884739e44f9498906
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-08-24 20:14:15 +0900

    자리 배치 오류 해결

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_082601) do
+ActiveRecord::Schema.define(version: 2018_08_24_090640) do
 
   create_table "images", force: :cascade do |t|
     t.string "alt"
