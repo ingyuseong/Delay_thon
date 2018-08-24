a60f84addb34ee6ef8f937188e99a0d3909acae8
JIHYUN1110 <jodyzzang77@likelion.org>
2018-08-24 21:34:31 +0900

    index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_082601) do
+ActiveRecord::Schema.define(version: 2018_08_24_090640) do
 
   create_table "images", force: :cascade do |t|
     t.string "alt"
@@ -49,6 +49,9 @@ ActiveRecord::Schema.define(version: 2018_08_24_082601) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.string "status"
+    t.string "summary"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "row"
